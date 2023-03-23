84ddb26e9eac367e99622439d6c01b6d96d380af
Martynas Slavinskas <Martynas.Slavinskas@gmail.com>
2023-03-23 21:56:48 +0200

    changed enemy spawning logic

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -27,6 +27,12 @@ public class EnemySpawner : MonoBehaviour
     {
         Vector3 spawnPos = GetRandomSpawnPosition();
 
+        // Adjust the spawn position to be outside the camera view
+        while (IsInsideCameraView(spawnPos))
+        {
+            spawnPos += (spawnPos - Camera.main.transform.position).normalized * spawnDistanceFromCamera;
+        }
+
         // Check if spawn position is on a collidable tile
         if (collidableTilemap.GetTile(collidableTilemap.WorldToCell(spawnPos)))
         {
@@ -42,26 +48,39 @@ public class EnemySpawner : MonoBehaviour
         Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
     }
 
-    private Vector3 GetRandomSpawnPosition()
+    private bool IsInsideCameraView(Vector3 position)
     {
-
         float cameraHeight = 2f * Camera.main.orthographicSize;
         float cameraWidth = cameraHeight * Camera.main.aspect;
 
         Vector3 cameraPos = Camera.main.transform.position;
 
-        // Calculate a random position on the edge of the camera view
+        float distanceX = Mathf.Abs(cameraPos.x - position.x);
+        float distanceY = Mathf.Abs(cameraPos.y - position.y);
+
+        return distanceX < cameraWidth / 2f && distanceY < cameraHeight / 2f;
+    }
+
+    private Vector3 GetRandomSpawnPosition()
+    {
+        float cameraHeight = 2f * Camera.main.orthographicSize;
+        float cameraWidth = cameraHeight * Camera.main.aspect;
+
+        Vector3 cameraPos = Camera.main.transform.position;
+
+        // Calculate a random position on the edge of the camera view that is at least spawnDistanceFromCamera units away from the camera
         Vector3 randomEdgePos = new Vector3(
             cameraPos.x + Random.Range(-cameraWidth / 2f, cameraWidth / 2f),
             cameraPos.y + Random.Range(-cameraHeight / 2f, cameraHeight / 2f),
             0f);
 
-        // Calculate the direction towards the camera
         Vector3 towardsCamera = (cameraPos - randomEdgePos).normalized;
 
+        // Calculate the minimum distance from the camera that the enemy can spawn
+        float minDistanceFromCamera = spawnDistanceFromCamera + enemyPrefab.GetComponent<Renderer>().bounds.extents.magnitude;
+
         // Calculate the spawn position by moving it in the direction away from the camera
-        Vector3 spawnPos = randomEdgePos + new Vector3(towardsCamera.x, towardsCamera.y, 0f) * spawnDistanceFromCamera;
-        spawnPos.z = 0;
+        Vector3 spawnPos = randomEdgePos + new Vector3(towardsCamera.x, towardsCamera.y, 0f) * minDistanceFromCamera;
 
         return spawnPos;
     }
