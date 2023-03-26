00e0e2f0800e2af1c9ac3418b686bb115b27da27
Damynas <povdam6@gmail.com>
2023-03-26 22:44:53 +0300

    Initial Main Menu Created

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    public void NewGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("Quit Game!");
+        Application.Quit();
+    }
+}
